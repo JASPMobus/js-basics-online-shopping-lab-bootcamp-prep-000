8c499cd4a6ebdea28c9a1409be7ab75dbdb2f1ec
James Mobus <mobusj@live.com>
2019-07-23 22:23:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,15 +10,56 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  // write your code here
+  var obj = { itemName : [item],
+              itemPrice : Math.floor(Math.random() * 100)}
+ cart.push(obj)
+ 
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  //you have nothing in your cart
+  if(cart.length===0) {
+    return "Your shopping cart is empty."
+  }
+  
+  //you have something in your cart, then
+  else {
+    var ret = "In your cart, you have"
+    
+    for(let i=0;i<cart.length;i++) {
+      //if you're on the last item, add an "and" in
+      if(i===cart.length-1 && cart.length!=1) {
+        ret = ret + " and"
+      }
+      
+      //main statement addition
+      ret = ret + ` ${cart[i].itemName} at $${cart[i].itemPrice}`
+      
+      //adding in punctuation
+      if(i===cart.length-1) {
+        ret = ret + "."
+      }
+      
+      else {
+        ret = ret + ","
+      }
+    }
+    
+    return ret
+  }
 }
 
 function total() {
-  // write your code here
+  //loop through everything in the cart, and add their price to ret
+  var ret = 0
+  
+  for(let i = 0;i<cart.length;i++) {
+    ret = ret + cart[i].itemPrice
+  }
+  
+  return ret
 }
 
 function removeFromCart(item) {
