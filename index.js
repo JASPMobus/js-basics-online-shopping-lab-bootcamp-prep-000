03cc2433a894caba9c24b9754b2d458934a703d5
James Mobus <mobusj@live.com>
2019-07-23 23:05:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function removeFromCart(item) {
   //loops through the array to cut out anything that has the given name
   for(let i=0;i<cart.length;i++) {
     if(cart[i].itemName===item) {
-      return cart.splice(i, 1)
+      cart.splice(i, 1)
     }
   }
   
